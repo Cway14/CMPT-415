ff42e89bc1d41e123818f599e3ebda7953d0cc5f
Cam Way <cway140@gmail.com>
2023-02-27 11:05:59 -0800

    connected to firebase

--- client/src/components/LoginModal/LoginModal.jsx
+++ client/src/components/LoginModal/LoginModal.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import LoginModalTemplate from "./LoginModalTemplate";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginModal = (props) => {
+
+  const auth = getAuth();
+  function signIn(credentials) {
+    const { email, password } = credentials;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        alert("Signed in as: " + user.email)
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("ERROR: ", errorMessage)
+      });
+  }
   return (
     <LoginModalTemplate
       submitButtonText="Login"
-      submitButtonAction={() => console.log("Login button clicked")}
+      submitButtonAction={(credentials) => signIn(credentials)}
       headerImage="crown"
       showModal={props.showModal}
       hideModal={props.hideModal}
--- client/src/components/LoginModal/SignUpModal/SignUpModal.jsx
+++ client/src/components/LoginModal/SignUpModal/SignUpModal.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import LoginModalTemplate from "../LoginModalTemplate";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUpModal = (props) => {
+
+
+  const auth = getAuth();
+  function createUser(credentials){
+    const { email, password } = credentials;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+  }
+  
   return (
     <LoginModalTemplate
       submitButtonText="Sign up"
-      submitButtonAction={() => console.log("Sign up button clicked")}
+      submitButtonAction={(credentials) => createUser(credentials)}
       headerImage="signUp"
       showModal={props.showModal}
       hideModal={props.hideModal}
